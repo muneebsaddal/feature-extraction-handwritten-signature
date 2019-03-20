9272e536b47abeb52efb3de504c2f1ad779066be
Muneeb Saddal <muneebsaddal@outlook.com>
2019-03-20 21:30:52 +0500

    caluclating normal value

--- signAnalysis.py
+++ signAnalysis.py
@@ -41,14 +41,15 @@ def findCentroid(img, left, right, top, bottom):
     draw.rectangle(((left, cy), (cx, bottom)))
     draw.rectangle(((cx, cy), (right, bottom)))
     #Writing output to file
-    h.write("%d, " % cx)
-    h.write("%d\n" % cy)
+    #h.write("%d, " % cx)
+    #h.write("%d\n" % cy)
     return cx, cy
 
 #Function to find the transitions of image
-def findTransitions(img, left, right, top, bottom, cx, cy):
+def transitions_normalValue(img, left, right, top, bottom, cx, cy):
     prev = img.getpixel((0,0))
     n = 0
+    normalized = []
     transitions = []
     #Calling function to calculate transitions for each segment around centroid
     for x in range(left, cx):
@@ -57,39 +58,43 @@ def findTransitions(img, left, right, top, bottom, cx, cy):
             if curr is 255 and prev is 0:
                 n = n + 1
             prev = curr
+    normalized.append(((cx-left)*(cy-top))/n)
     transitions.append(n)
     for x in range(cx, right):
-        for y in range(cy, top):
+        for y in range(top, cy):
             curr = img.getpixel((x,y))
             if curr is 255 and prev is 0:
                 n = n + 1
             prev = curr
+    normalized.append(((right-cx)*(cy-top))/n)
     transitions.append(n)
     for x in range(left, cx):
-        for y in range(bottom, cy):
+        for y in range(cy, bottom):
             curr = img.getpixel((x,y))
             if curr is 255 and prev is 0:
                 n = n + 1
             prev = curr
+    normalized.append(((cx-left)*(bottom-cy))/n)
     transitions.append(n)
     for x in range(cx, right):
-        for y in range(bottom, cy):
+        for y in range(cy, bottom):
             curr = img.getpixel((x,y))
             if curr is 255 and prev is 0:
                 n = n + 1
             prev = curr
+    normalized.append(((right-cx)*(bottom-cy))/n)
     transitions.append(n)
     #Writing output to file
-    for i in transitions:
-            g.write(" %d " % i)
-    g.write("\n")
-    return transitions
+    #for i in transitions:
+    #        g.write(" %d " % i)
+    #g.write("\n")
+    return transitions, normalized
 
 #Function to find the ratio
 def findRatio(left, right, top, bottom): 
     ratio = (right - left)/(bottom - top)
     #Writing output to file
-    f.write("%d\n" % ratio)
+    #f.write("%d\n" % ratio)
     return ratio
 
 # Function to split the image recursively
@@ -101,13 +106,14 @@ def split(img, left, right, top, bottom, depth=0):
         split(img, left, cx, cy, bottom, depth + 1)
         split(img, cx, right, cy, bottom, depth + 1)
     else:
-        t = findTransitions(img, left, right, top, bottom, cx, cy)
+        t, norm = transitions_normalValue(img, left, right, top, bottom, cx, cy)
         r = findRatio(left, right, top, bottom)
        
 if __name__=="__main__":
-    f = open("ratio.txt", "a+")
-    g = open("transitions.txt", "a+")
-    h = open("centroid.txt", "a+")
+    #f = open("ratio.txt", "a+")
+    #g = open("transitions.txt", "a+")
+    #h = open("centroid.txt", "a+")
+    i = open("normalValue.txt", "a+")
     #Convert signature to a binary image
     img = Image.open('sign0.jpg')
     img = img.convert('L').point((lambda x : 255 if x > 150 else 0), mode = '1')
@@ -116,8 +122,9 @@ if __name__=="__main__":
     draw = ImageDraw.Draw(img)
     #Function call to split image into 64 segments according to centroid
     split(img, left, right, top, bottom)
-    f.close()
-    g.close()
-    h.close()
+    #f.close()
+    #g.close()
+    #h.close()
+    i.close()
     #Show output image
-    #img.show()
+    #img.save("output.jpg")
